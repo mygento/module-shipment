4886f7d831943fb5a885d0997cea8c42687f9715
nikita tarasov <nikita@mygento.ru>
2020-03-12 15:20:54 +0300

    delivery date order view

---
+++ Block/Adminhtml/Order/DeliveryDatetime.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2016-2020 Mygento (https://www.mygento.ru)
+ * @package Mygento_Shipment
+ */
+
+namespace Mygento\Shipment\Block\Adminhtml\Order;
+
+class DeliveryDatetime extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
+{
+    /**
+     * Prepare html output
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (!$this->getOrder()->getShippingAddress()) {
+            return parent::_toHtml();
+        }
+
+        return parent::_toHtml();
+    }
+}
---
+++ Plugin/OrderGet.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2016-2020 Mygento (https://www.mygento.ru)
+ * @package Mygento_Shipment
+ */
+
+namespace Mygento\Shipment\Plugin;
+
+class OrderGet
+{
+    /**
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $subject
+     * @param \Magento\Sales\Api\Data\OrderInterface $result
+     *
+     * @return \Magento\Sales\Api\Data\OrderInterface
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGet(
+        \Magento\Sales\Api\OrderRepositoryInterface $subject,
+        \Magento\Sales\Api\Data\OrderInterface $result
+    ) {
+        return $this->getOrderAddress($result);
+    }
+
+    /**
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $subject
+     * @param \Magento\Sales\Model\ResourceModel\Order\Collection $resultOrder
+     * @return \Magento\Sales\Model\ResourceModel\Order\Collection
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGetList(
+        \Magento\Sales\Api\OrderRepositoryInterface $subject,
+        \Magento\Sales\Model\ResourceModel\Order\Collection $resultOrder
+    ) {
+        /** @var $order */
+        foreach ($resultOrder->getItems() as $order) {
+            $this->afterGet($subject, $order);
+        }
+
+        return $resultOrder;
+    }
+
+    /**
+     * @param \Magento\Sales\Api\Data\OrderInterface $order
+     * @return \Magento\Sales\Api\Data\OrderInterface
+     */
+    private function getOrderAddress(\Magento\Sales\Api\Data\OrderInterface $order)
+    {
+        $shippingAddress = $order->getShippingAddress();
+        $shippingAddress->getExtensionAttributes()->setDeliveryEstimate($shippingAddress->getDeliveryEstimate());
+        $shippingAddress->getExtensionAttributes()->setDeliveryDate($shippingAddress->getDeliveryDate());
+        $shippingAddress->getExtensionAttributes()->setDeliveryTimeFrom($shippingAddress->getDeliveryTimeFrom());
+        $shippingAddress->getExtensionAttributes()->setDeliveryTimeTo($shippingAddress->getDeliveryTimeTo());
+
+        return $order;
+    }
+}

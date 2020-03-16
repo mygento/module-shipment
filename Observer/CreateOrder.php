641bb5371da1b2e7cf71f008dad4546d2f84abb2
nikita tarasov <nikita@mygento.ru>
2020-03-16 16:49:43 +0300

    delivery date create

--- Observer/CreateOrder.php
+++ Observer/CreateOrder.php
@@ -26,19 +26,34 @@ class CreateOrder implements \Magento\Framework\Event\ObserverInterface
         }
 
         $extensionAttributes = $quote->getShippingAddress()->getExtensionAttributes();
-        $data = $request['estimate'] ?? false;
-        if (!$data) {
+        if (isset($request['order']['shipping_method']) || isset($request['collect_shipping_rates'])) {
             $extensionAttributes->setDeliveryDate(null);
+            $quote->getShippingAddress()->setDeliveryDate(null);
             $extensionAttributes->setDeliveryTimeFrom(null);
+
+            $quote->getShippingAddress()->setDeliveryTimeFrom(null);
             $extensionAttributes->setDeliveryTimeTo(null);
+
+            $quote->getShippingAddress()->setDeliveryTimeTo(null);
             $quote->getShippingAddress()->setExtensionAttributes($extensionAttributes);
 
             return;
         }
 
+        if (!isset($request['estimate'])) {
+            return;
+        }
+        $data = $request['estimate'];
+
         $extensionAttributes->setDeliveryDate($data['date'] ?? null);
+        $quote->getShippingAddress()->setDeliveryDate($data['date'] ?? null);
+
         $extensionAttributes->setDeliveryTimeFrom($data['time_from'] ?? null);
+        $quote->getShippingAddress()->setDeliveryTimeFrom($data['time_from'] ?? null);
+
         $extensionAttributes->setDeliveryTimeTo($data['time_to'] ?? null);
+        $quote->getShippingAddress()->setDeliveryTimeTo($data['time_to'] ?? null);
+
         $quote->getShippingAddress()->setExtensionAttributes($extensionAttributes);
     }
 }

c7c86fa4134d3054e7a9448c6a79318e732a3c49
nikita tarasov <nikita@mygento.ru>
2020-04-10 19:06:30 +0300

    delivery tab

---
+++ Controller/Adminhtml/Order/DeliveryTab.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2016-2020 Mygento (https://www.mygento.ru)
+ * @package Mygento_Shipment
+ */
+
+namespace Mygento\Shipment\Controller\Adminhtml\Order;
+
+use Magento\Backend\App\Action;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+abstract class DeliveryTab extends \Magento\Sales\Controller\Adminhtml\Order
+{
+    /**
+     * @var string
+     */
+    protected $block;
+
+    /**
+     * @var string
+     */
+    protected $template = 'order/view/tab/delivery.phtml';
+
+    /**
+     * @var \Magento\Framework\View\LayoutFactory
+     */
+    private $layoutFactory;
+
+    /**
+     * @param \Magento\Framework\View\LayoutFactory $layoutFactory
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Framework\App\Response\Http\FileFactory $fileFactory
+     * @param \Magento\Framework\Translate\InlineInterface $translateInline
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+     * @param OrderManagementInterface $orderManagement
+     * @param OrderRepositoryInterface $orderRepository
+     * @param LoggerInterface $logger
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+     */
+    public function __construct(
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
+        \Magento\Framework\Translate\InlineInterface $translateInline,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        OrderManagementInterface $orderManagement,
+        OrderRepositoryInterface $orderRepository,
+        LoggerInterface $logger
+    ) {
+        parent::__construct(
+            $context,
+            $coreRegistry,
+            $fileFactory,
+            $translateInline,
+            $resultPageFactory,
+            $resultJsonFactory,
+            $resultLayoutFactory,
+            $resultRawFactory,
+            $orderManagement,
+            $orderRepository,
+            $logger
+        );
+        $this->layoutFactory = $layoutFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\Result\Raw
+     */
+    public function execute()
+    {
+        $this->_initOrder();
+        $layout = $this->layoutFactory->create();
+
+        $block = $layout->createBlock($this->block);
+        $block->setTemplate($this->template);
+        $html = $block->toHtml();
+
+        $this->_translateInline->processResponseBody($html);
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents($html);
+
+        return $resultRaw;
+    }
+}

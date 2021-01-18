648ea84e9948df21b3ab135b9b1c2777428dd335
nikita tarasov <nikita@mygento.ru>
2021-01-18 12:52:05 +0300

    marking update

---
+++ Api/Service/MarkingInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2016-2020 Mygento (https://www.mygento.ru)
+ * @package Mygento_Shipment
+ */
+
+namespace Mygento\Shipment\Api\Service;
+
+interface MarkingInterface
+{
+    public function updateCarrierMarking(\Magento\Sales\Model\Order $order);
+}
